4503aaaeb43c81124058d8c059e1a704b5a49ef8
Siddharth Thakurdesai <siddharthtd@gmail.com>
2020-04-22 21:53:06 -0700

    Added another build step for container deployment

--- flask-docker.py
+++ flask-docker.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
-	return "Flask File modified"
+	return "Flask File modified. Build Completed"
 
 if __name__ == '__main__':
 	app.run(debug=True,host='0.0.0.0')

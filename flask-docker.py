e8c8d84b53f39e16cecc0fa37ce38612af98a6da
Siddharth Thakurdesai <siddharthtd@gmail.com>
2020-04-22 21:42:55 -0700

    This commit should change the displayed text on the container

--- flask-docker.py
+++ flask-docker.py
@@ -1,14 +1,11 @@
 from flask import Flask
-import os
+
 app = Flask(__name__)
 
 @app.route('/')
 
 def hello_world():
-	var = "Flash Dockerized"
-	mod_var = "This is a new addition"
-	print(var)
-	return mod_var
+	return "Flask File modified"
 
 if __name__ == '__main__':
 	app.run(debug=True,host='0.0.0.0')

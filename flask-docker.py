86801dff21cd74ebdd63a2706ca8ac789a5bb15e
Siddharth Thakurdesai <siddharthtd@gmail.com>
2020-04-22 21:39:21 -0700

    This commit will trigger a build on jenkins server

--- flask-docker.py
+++ flask-docker.py
@@ -1,11 +1,14 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 @app.route('/')
 
 def hello_world():
-	return 'Flask Dockerized'
+	var = "Flash Dockerized"
+	mod_var = "This is a new addition"
+	print(var)
+	return mod_var
 
 if __name__ == '__main__':
 	app.run(debug=True,host='0.0.0.0')
